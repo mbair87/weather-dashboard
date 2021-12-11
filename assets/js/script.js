555c41f93f54093a389def88f46fc9b680f1da22
Michael Bair <mbair87@gmail.com>
2021-12-11 14:55:16 -0700

    added function to show daily weather and add divs for five day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var cities = [];
 var cityHistory =$("#city-history");
- 
+var days =$("#days");
+
 
 getCities();
 
@@ -38,7 +39,7 @@ function generateCities() {
 
 //click listener on search button
   $("#city-search").on("click", function(event){
-  
+  event.preventDefault();
   // get value from city input search field
   var city = $("#city-input").val().trim();
 
@@ -47,7 +48,55 @@ function generateCities() {
   console.log(city);
   saveCities();
   generateCities();
+  getWeather(city);
 });
+
+var getWeather = function(cityName) {
+    //get lat and lon coordinates for city
+    var geoUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=8251dbb2e3fad51d11bd788bdfe005a3";
+    $.get(geoUrl, function(response){
+        //add coordinates to API call
+        var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+response[0].lat +"&lon=" +response[0].lon+ "&units=imperial"+"&exclude=hourly,minutely,alerts&appid=8251dbb2e3fad51d11bd788bdfe005a3";
+        $.get(weatherUrl, function(response){
+          console.log(response);   
+          displayWeather(response, cityName);
+          displayFiveDay(response, cityName);
+        })
+    })
+};
+
+var displayWeather = function(weather, cityName) {
+    var iconUrl =`http://openweathermap.org/img/wn/${weather.current.weather[0].icon}.png` 
+    var currentWeather=$("#current-weather");
+    var h3=$("<h3>").text(cityName + " " + moment.unix(weather.current.dt).format('MMMM Do YYYY'));
+    // var currentDate=$("<h3>").text(moment.unix(weather.current.dt).format('MMMM Do YYYY, h:mm a'));
+    currentWeather.append(h3);
+    var currentIcon=$("<img>").attr("src", iconUrl);
+    h3.append(currentIcon);
+    // currentWeather.append(currentDate);
+    var currentTemp=$("<p>").text("Temperature: " + weather.current.temp + "°F");
+    currentWeather.append(currentTemp);
+    var currentWind=$("<p>").text("Wind: " + weather.current.wind_speed + " MPH");
+    currentWeather.append(currentWind);
+    var currentHumidity=$("<p>").text("Humidity: " + weather.current.humidity + "%");
+    currentWeather.append(currentHumidity);
+    var currentUv=$("<p>").text("UV Index: " + weather.current.uvi);
+    currentWeather.append(currentUv);
+} 
+
+var displayFiveDay = function(weather, cityName) {
+    for (var i = 1; i <= 5; i++) {
+        var dayWeather = weather.daily[i];
+        var dayDiv = $("<div>");
+        dayDiv.attr;("class","col-3 m-2 bg-primary")
+      var fiveDayh6= $("<h6>").text(moment.unix(weather.daily[i].dt).format('l'));
+dayDiv.append(fiveDayh6);
+      days.append(dayDiv);
+    }
+}
+
+
+
 
 
 
