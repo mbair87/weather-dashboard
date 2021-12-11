186136fa26a13273fa73a6eb5f133c25ca73fca0
Michael Bair <mbair87@gmail.com>
2021-12-11 08:26:39 -0700

    fixed city history to prepend not append

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,36 @@
 var cities = [];
-  
- //save cities to local storage
+var cityHistory =$("#city-history");
+ 
+
+getCities();
+
+// get cities from local storage
+function getCities(){ 
+    var savedCities = JSON.parse(localStorage.getItem("cities"));
+
+    // push any saved cities from localStorage to cities array
+    if (savedCities !== null) {
+        cities = savedCities;
+      }
+    generateCities();
+    
+    console.log(cities);
+}
+
+//create html elements for cities
+function generateCities() {
+ 
+    // create list elemet for each city
+    for (var i = 0; i < cities.length; i++) {
+      var city = cities[i];
+      
+      var li = $("<li>").text(city);
+      li.attr("class", "list-group-item");
+      cityHistory.prepend(li);
+    }
+}   
+
+//save cities to local storage
  function saveCities() {
      localStorage.setItem("cities", JSON.stringify(cities));
      console.log(localStorage);
@@ -11,11 +41,12 @@ var cities = [];
   
   // get value from city input search field
   var city = $("#city-input").val().trim();
-  
+
   //add city from input to cities array
   cities.push(city)
   console.log(city);
   saveCities();
+  generateCities();
 });
 
 
